ff4154e1791fd064f5ff997557754f94bdc480d0
rob barnsley <r.barnsley@skatelescope.org>
2020-09-16 15:37:29 +0000

    merge on prod

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ build:
 	@docker build . -f Dockerfile --tag rucio-analysis:latest
 
 build-dev:
-	@docker build . -f Dockerfile.dev --tag rucio-analysis:dev
+	@docker build . -f Dockerfile.dev --tag rucio-analysis:dev 
--- etc/cron/run-analysis.sh
+++ etc/cron/run-analysis.sh
@@ -4,7 +4,7 @@ now=`date +%Y-%m-%d.%H:%M:%S`
 
 echo $now > /tmp/log_$now
 
-docker exec -e -it rucio-analysis-prod /bin/bash \
-    -c 'voms-proxy-init --cert /opt/rucio/etc/client.crt --key /opt/rucio/etc/client.key --voms escape' | tee -a /tmp/log_$now
-docker exec -e RUCIO_ANALYSIS_ROOT=/opt/rucio-analysis -it rucio-analysis-prod /bin/bash \
-    -c 'python3 $RUCIO_ANALYSIS_ROOT/src/run-analysis.py -v -t $RUCIO_ANALYSIS_ROOT/etc/tests.yml' | tee -a /tmp/log_$now
+export RUCIO_ANALYSIS_ROOT=/opt/rucio-analysis
+
+docker exec -e -i rucio-analysis-prod voms-proxy-init --cert /opt/rucio/etc/client.crt --key /opt/rucio/etc/client.key --voms escape | tee -a /tmp/log_$now
+docker exec -e RUCIO_ANALYSIS_ROOT=$RUCIO_ANALYSIS_ROOT -i rucio-analysis-prod python3 $RUCIO_ANALYSIS_ROOT/src/run-analysis.py -v -t $RUCIO_ANALYSIS_ROOT/etc/tests.yml >> /tmp/log_$now
