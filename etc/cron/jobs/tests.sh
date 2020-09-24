68e87a1dbd8308f2994fbff15fc48b8361111eb4
Rob Barnsley <robbarnsley@users.noreply.github.com>
2020-09-24 16:42:14 +0100

    Pull tests.yml dynamic (#6)
    * added dynamic linking to tests.yml on github
    
    Co-authored-by: Rob Barnsley <R.Barnsley@skatelescope.org>

--- etc/cron/jobs/tests.sh
+++ etc/cron/jobs/tests.sh
@@ -3,4 +3,5 @@
 /etc/profile.d/rucio_init.sh
 
 voms-proxy-init --cert /opt/rucio/etc/client.crt --key /opt/rucio/etc/client.key --voms escape
-python3 /opt/rucio-analysis/src/run-analysis.py -v -t /opt/rucio-analysis/etc/tests.yml
+wget https://raw.githubusercontent.com/ESCAPE-WP2/rucio-analysis/master/etc/tests.yml -O /tmp/tests.yml
+python3 /opt/rucio-analysis/src/run-analysis.py -v -t /tmp/tests.yml
