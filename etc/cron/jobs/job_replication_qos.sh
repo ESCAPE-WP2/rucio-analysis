5201df676ee5ba5c11e4ec4cbbd40e7f58de7617
rob barnsley <r.barnsley@skatelescope.org>
2020-10-23 17:49:08 +0000

    moving to statically linked yml

--- etc/cron/jobs/job_replication_qos.sh
+++ etc/cron/jobs/job_replication_qos.sh
@@ -3,5 +3,5 @@
 /etc/profile.d/rucio_init.sh
 
 voms-proxy-init --cert /opt/rucio/etc/client.crt --key /opt/rucio/etc/client.key --voms escape
-wget https://raw.githubusercontent.com/ESCAPE-WP2/rucio-analysis/master/etc/test_replication_qos.yml -O /tmp/test_replication_qos.yml
-python3 /opt/rucio-analysis/src/run-analysis.py -t /tmp/test_replication_qos.yml
+#wget https://raw.githubusercontent.com/ESCAPE-WP2/rucio-analysis/master/etc/test_replication_qos.yml -O /tmp/test_replication_qos.yml
+python3 /opt/rucio-analysis/src/run-analysis.py -t /opt/rucio-analysis/etc/test_replication_qos.yml
--- etc/cron/jobs/job_sync_database.sh
+++ etc/cron/jobs/job_sync_database.sh
@@ -3,5 +3,5 @@
 /etc/profile.d/rucio_init.sh
 
 voms-proxy-init --cert /opt/rucio/etc/client.crt --key /opt/rucio/etc/client.key --voms escape
-wget https://raw.githubusercontent.com/ESCAPE-WP2/rucio-analysis/master/etc/sync_database.yml -O /tmp/sync_database.yml
-python3 /opt/rucio-analysis/src/run-analysis.py -t /tmp/sync_database.yml
+#wget https://raw.githubusercontent.com/ESCAPE-WP2/rucio-analysis/master/etc/sync_database.yml -O /tmp/sync_database.yml
+python3 /opt/rucio-analysis/src/run-analysis.py -t /opt/rucio-analysis/etc/sync_database.yml
