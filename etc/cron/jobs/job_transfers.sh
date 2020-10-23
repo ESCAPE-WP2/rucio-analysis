f9af92d46ab38641faa7952aee995ebf2cd157f0
Rob Barnsley <R.Barnsley@skatelescope.org>
2020-10-23 14:59:54 +0100

    added logrotate

--- etc/cron/jobs/job_transfers.sh
+++ etc/cron/jobs/job_transfers.sh
@@ -4,4 +4,4 @@
 
 voms-proxy-init --cert /opt/rucio/etc/client.crt --key /opt/rucio/etc/client.key --voms escape
 wget https://raw.githubusercontent.com/ESCAPE-WP2/rucio-analysis/master/etc/test_transfers.yml -O /tmp/test_transfers.yml
-python3 /opt/rucio-analysis/src/run-analysis.py -v -t /tmp/test_transfers.yml
+python3 /opt/rucio-analysis/src/run-analysis.py -t /tmp/test_transfers.yml
