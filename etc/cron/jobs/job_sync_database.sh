930a2d43cf8a55f7e3ddd8edbaee68609b2515d4
Rob Barnsley <R.Barnsley@skatelescope.org>
2020-10-23 13:49:55 +0100

    update crontab

---
+++ etc/cron/jobs/job_sync_database.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/etc/profile.d/rucio_init.sh
+
+voms-proxy-init --cert /opt/rucio/etc/client.crt --key /opt/rucio/etc/client.key --voms escape
+wget https://raw.githubusercontent.com/ESCAPE-WP2/rucio-analysis/master/etc/sync_database.yml -O /tmp/sync_database.yml
+python3 /opt/rucio-analysis/src/run-analysis.py -v -t /tmp/sync_database.yml
