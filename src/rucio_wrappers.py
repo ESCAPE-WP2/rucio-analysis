c7b2afeab3c14a1972f5dce45b3488a03be37abc
Rob Barnsley <robbarnsley@users.noreply.github.com>
2020-10-28 16:40:28 +0000

    adding error fields for uploads in database (#16)
    Co-authored-by: Rob Barnsley <R.Barnsley@skatelescope.org>

--- src/rucio_wrappers.py
+++ src/rucio_wrappers.py
@@ -202,14 +202,12 @@ class RucioWrappersCLI(RucioWrappers):
 class RucioWrappersAPI(RucioWrappers):
     @staticmethod
     def addDataset(did):
-        try:
-            client = Client()
-            tokens = did.split(":")
-            scope = tokens[0]
-            name = tokens[1]
-            client.add_dataset(scope=scope, name=name)
-        except RucioException as error:
-            raise Exception(error)
+        client = Client()
+        tokens = did.split(":")
+        scope = tokens[0]
+        name = tokens[1]
+        client.add_dataset(scope=scope, name=name)
+
 
     @staticmethod
     def addRule(
@@ -218,185 +216,147 @@ class RucioWrappersAPI(RucioWrappers):
         tokens = did.split(":")
         scope = tokens[0]
         name = tokens[1]
-        try:
-            client = Client()
-            rtn = client.add_replication_rule(
-                dids=[{"scope": scope, "name": name}],
-                copies=copies,
-                rse_expression=dst,
-                lifetime=lifetime,
-                activity=activity,
-                source_replica_expression=src,
-            )
-            return rtn
-        except RucioException as error:
-            raise Exception(error)
+
+        client = Client()
+        rtn = client.add_replication_rule(
+            dids=[{"scope": scope, "name": name}],
+            copies=copies,
+            rse_expression=dst,
+            lifetime=lifetime,
+            activity=activity,
+            source_replica_expression=src,
+        )
+        return rtn
 
     @staticmethod
     def attach(todid, dids):
-        try:
-            client = Client()
-            tokens = todid.split(":")
-            toScope = tokens[0]
-            toName = tokens[1]
+        client = Client()
+        tokens = todid.split(":")
+        toScope = tokens[0]
+        toName = tokens[1]
 
-            attachment = {"scope": toScope, "name": toName, "dids": []}
-            for did in dids.split(" "):
-                tokens = did.split(":")
-                scope = tokens[0]
-                name = tokens[1]
-                attachment["dids"].append({"scope": scope, "name": name})
-            client.attach_dids_to_dids(attachments=[attachment])
-        except RucioException as error:
-            raise Exception(error)
+        attachment = {"scope": toScope, "name": toName, "dids": []}
+        for did in dids.split(" "):
+            tokens = did.split(":")
+            scope = tokens[0]
+            name = tokens[1]
+            attachment["dids"].append({"scope": scope, "name": name})
+        client.attach_dids_to_dids(attachments=[attachment])
 
     @staticmethod
     def detach(fromdid, dids):
-        try:
-            client = Client()
-            tokens = fromdid.split(":")
-            fromScope = tokens[0]
-            fromName = tokens[1]
+        client = Client()
+        tokens = fromdid.split(":")
+        fromScope = tokens[0]
+        fromName = tokens[1]
 
-            detachments = []
-            for did in dids.split(" "):
-                tokens = did.split(":")
-                scope = tokens[0]
-                name = tokens[1]
-                detachments.append({"scope": scope, "name": name})
-            client.detach_dids(scope=fromScope, name=fromName, dids=detachments)
-        except RucioException as error:
-            raise Exception(error)
+        detachments = []
+        for did in dids.split(" "):
+            tokens = did.split(":")
+            scope = tokens[0]
+            name = tokens[1]
+            detachments.append({"scope": scope, "name": name})
+        client.detach_dids(scope=fromScope, name=fromName, dids=detachments)
 
     @staticmethod
     def download(did, baseDir="download", logger=None):
-        try:
-            client = DownloadClient(logger=logger)
-            items = [{"did": did, "base_dir": baseDir}]
-            client.download_dids(items=items)
-        except RucioException as error:
-            raise Exception(error)
+        client = DownloadClient(logger=logger)
+        items = [{"did": did, "base_dir": baseDir}]
+        client.download_dids(items=items)
 
     @staticmethod
     def erase(did):
-        try:
-            api = RucioWrappersAPI()
-            rules = api.listReplicationRules(did)
-            rids = set()
-            for rule in rules:
-                rids.add(rule["id"])
-            client = Client()
-            for rid in rids:
-                client.delete_replication_rule(rule_id=rid, purge_replicas=True)
-        except RucioException as error:
-            raise Exception(error)
+        api = RucioWrappersAPI()
+        rules = api.listReplicationRules(did)
+        rids = set()
+        for rule in rules:
+            rids.add(rule["id"])
+        client = Client()
+        for rid in rids:
+            client.delete_replication_rule(rule_id=rid, purge_replicas=True)
 
     @staticmethod
     def getMetadata(did):
-        try:
-            client = Client()
-            tokens = did.split(":")
-            scope = tokens[0]
-            name = tokens[1]
-            metadata = client.get_metadata(scope, name)
-            return metadata
-        except RucioException as error:
-            raise Exception(error)
+        client = Client()
+        tokens = did.split(":")
+        scope = tokens[0]
+        name = tokens[1]
+        metadata = client.get_metadata(scope, name)
+        return metadata
 
     @staticmethod
     def listDIDs(scope, name="*", filters=None):
-        try:
-            client = Client()
-            filters_dict = {}
-            if filters is not None:
-                if isinstance(filters, str):
-                    for pair in filters.split(","):
-                        tokens = pair.split("=")
-                        key = tokens[0].strip()
-                        val = tokens[1].strip()
-                        filters_dict[key] = val
-                else:
-                    filters_dict = filters
-            dids = []
-            for name in client.list_dids(scope=scope, filters=filters_dict):
-                dids.append('{}:{}'.format(scope, name))
-            return dids
-        except RucioException as error:
-            raise Exception(error)
+        client = Client()
+        filters_dict = {}
+        if filters is not None:
+            if isinstance(filters, str):
+                for pair in filters.split(","):
+                    tokens = pair.split("=")
+                    key = tokens[0].strip()
+                    val = tokens[1].strip()
+                    filters_dict[key] = val
+            else:
+                filters_dict = filters
+        dids = []
+        for name in client.list_dids(scope=scope, filters=filters_dict):
+            dids.append('{}:{}'.format(scope, name))
+        return dids
 
     @staticmethod
     def listFileReplicas(did, rse=None):
-        try:
-            client = Client()
-            tokens = did.split(":")
-            scope = tokens[0]
-            name = tokens[1]
-            replicas = []
-            for replica in client.list_replicas(dids=[{"scope": scope, "name": name}],
-            rse_expression=rse):
-                replicas.append(replica)
-            return replicas
-        except RucioException as error:
-            raise Exception(error)
+        client = Client()
+        tokens = did.split(":")
+        scope = tokens[0]
+        name = tokens[1]
+        replicas = []
+        for replica in client.list_replicas(dids=[{"scope": scope, "name": name}],
+        rse_expression=rse):
+            replicas.append(replica)
+        return replicas
 
     @staticmethod
     def listReplicationRules(did):
-        try:
-            client = Client()
-            tokens = did.split(":")
-            scope = tokens[0]
-            name = tokens[1]
-            rules = []
-            for rule in client.list_replication_rules(
-                filters={"scope": scope, "name": name}
-            ):
-                rules.append(rule)
-            return rules
-        except RucioException as error:
-            raise Exception(error)
+        client = Client()
+        tokens = did.split(":")
+        scope = tokens[0]
+        name = tokens[1]
+        rules = []
+        for rule in client.list_replication_rules(
+            filters={"scope": scope, "name": name}
+        ):
+            rules.append(rule)
+        return rules
 
     @staticmethod
     def listReplicationRulesFull(did):
-        try:
-            client = Client()
-            tokens = did.split(":")
-            scope = tokens[0]
-            name = tokens[1]
-            rules = []
-            for rule in client.list_replication_rule_full_history(
-                scope=scope, name=name
-            ):
-                rules.append(rule)
-            return rules
-        except RucioException as error:
-            raise Exception(error)
+        client = Client()
+        tokens = did.split(":")
+        scope = tokens[0]
+        name = tokens[1]
+        rules = []
+        for rule in client.list_replication_rule_full_history(
+            scope=scope, name=name
+        ):
+            rules.append(rule)
+        return rules
 
     @staticmethod
     def listRSEs(rse=None):
-        try:
-            client = Client()
-            rses = client.list_rses(rse)
-            return rses
-        except RucioException as error:
-            raise Exception(error)
+        client = Client()
+        rses = client.list_rses(rse)
+        return rses
 
     @staticmethod
     def listRSEAttributes(rse):
-        try:
-            client = Client()
-            rseDict = client.list_rse_attributes(rse)
-            return rseDict
-        except RucioException as error:
-            raise Exception(error)
+        client = Client()
+        rseDict = client.list_rse_attributes(rse)
+        return rseDict
 
     @staticmethod
     def ruleInfo(ruleID):
-        try:
-            client = Client()
-            info = client.get_replication_rule(ruleID)
-            return info
-        except RucioException as error:
-            raise Exception(error)
+        client = Client()
+        info = client.get_replication_rule(ruleID)
+        return info
 
     @staticmethod
     def upload(rse, scope, filePath, lifetime, registerAfterUpload=True,
@@ -413,16 +373,11 @@ class RucioWrappersAPI(RucioWrappers):
                 "transfer_timeout": transferTimeout
             }
         )
-        try:
-            client = UploadClient(logger=logger)
-            client.upload(items=items)
-        except RucioException as error:
-            raise Exception(error)
+        client = UploadClient(logger=logger)
+        client.upload(items=items)
 
     @staticmethod
     def whoAmI():
-        try:
-            client = Client()
-            return client.whoami()
-        except RucioException as error:
-            raise Exception(error)
+        client = Client()
+        return client.whoami()
+
