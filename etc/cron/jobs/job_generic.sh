a44ad407f129132ad3e0ef9d11ce2fccfd28b9e6
Rohini Joshi <r.joshi@skatelescope.org>
2020-11-20 12:23:59 +0000

    add high throughput test yaml and a generic job script

---
+++ etc/cron/jobs/job_generic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/etc/profile.d/rucio_init.sh
+
+voms-proxy-init --cert /opt/rucio/etc/client.crt --key /opt/rucio/etc/client.key --voms escape
+#wget https://raw.githubusercontent.com/ESCAPE-WP2/rucio-analysis/master/etc/test_replication_bulk.yml -O /tmp/test_replication_bulk.yml
+python3 /opt/rucio-analysis/src/run-analysis.py -v -t $1
